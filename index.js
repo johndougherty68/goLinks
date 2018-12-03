7d3a9f0f1d66e4ec27398ebe10e6bd19d328aece
johndougherty68 <johndougherty68@gmail.com>
2018-12-03 09:52:17 -0500

    Nov30

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger = require('./logger');
 const urlMapper = require('./urlMapper');
 const path = require("path");
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,28 +17,21 @@ app.use(morgan('dev', {
     }, stream: logger.stream
 }));
 
+//get the whole list of links
 app.get('/links', function(req, res){
     logger.info("getting list of links");
     const links = urlMapper.all();
     res.json(links);
 })
 
-app.get('/admin/:link?', function (req, res) {
+//get the admin page
+app.get('/admin', function (req, res) {
     logger.info("admin " + req.url);
     const link = req.params.link;
-    if(link)
-    {
-        // const mappedURL = urlMapper.get(link);
-        // if(mappedURL)
-        // {
-        //     res.redirect(mappedURL);
-        // }
-    }
-    else{
-        res.sendFile(path.join(__dirname, "./public/links.html"));
-    }
+    res.sendFile(path.join(__dirname, "./public/links.html"));
 })
 
+//delete the given goLink
 app.delete('/admin/link', function(req, res){
     console.log(req.body);
     logger.info("/admin/link " + req.body.goLink + " " + req.body.url);
@@ -46,13 +39,14 @@ app.delete('/admin/link', function(req, res){
     res.json(req.body);
 })
 
-
+//update or create goLink
 app.post('/admin/link', function(req, res){
     logger.info("/admin/link " + req.body.goLink + " " + req.body.url);
     urlMapper.set(req.body.goLink, req.body.url);
     res.json(req.body);
 })
 
+//get the url for the given goLink 
 app.get('/:link?', function (req, res) {
     logger.info("get " + req.url);
     const link = req.params.link;
